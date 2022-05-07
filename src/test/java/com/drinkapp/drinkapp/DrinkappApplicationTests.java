be0b22430a506e4a4109e0da19c887dcce7a531d
Klaudia2205 <63544097+Klaudia2205@users.noreply.github.com>
2022-05-07 19:03:26 +0200

    vytvorenie Spring projektu

---
+++ src/test/java/com/drinkapp/drinkapp/DrinkappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.drinkapp.drinkapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DrinkappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
